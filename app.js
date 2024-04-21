c3491dddb0122ec44be78e84d44233873d031f2a
Ankit <ankitsharmgh093@gmail.com>
2024-04-21 18:47:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: "http://92.168.155.187:5173/",
+});
+
+io.on("connection", (socket) => {
+  console.log("someone connected");
+  socket.on("mouse-move", (data) => {
+    socket.broadcast.emit("mouse movement", data);
+  });
+  socket.on("drawing", (data) => {
+    socket.broadcast.emit("user drawn", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("someone disconnected");
+    socket.broadcast.emit("user disconnected", null);
+  });
+});
+
+server.listen(8000, () => {
+  console.log("server running at http://localhost:8000 🔥");
+});
